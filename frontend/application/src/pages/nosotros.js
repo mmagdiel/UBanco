b7f12b27b358f162fbc9a488436d6c2291eedfa3
Magdiel Marquez <magdielikari@gmail.com>
2020-05-18 10:43:58 -0500

    Add cancelar, informacion, nosotros, registro pages

---
+++ frontend/application/src/pages/nosotros.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import Navbar from "../../../desing-system/src/components/molecules/navbar"
+import Logo from "../../../desing-system/src/components/atoms/logo"
+import Nav from "../../../desing-system/src/components/molecules/nav"
+
+const Nosotros = () => {
+  const [close, toggleClose] = useState(false)
+  return (
+    <Navbar close={close} toggleClose={toggleClose}>
+      <Link className="navbar-brand" to="/">
+        <Logo />
+      </Link>
+      <Nav close={close}>
+        <Link className="nav-link" to="/informacion">
+          Información
+        </Link>
+        <Link className="nav-link active" to="/nosotros">
+          Nosotros
+        </Link>
+        <Link className="nav-link" to="/cancelar">
+          Cancelar
+        </Link>
+      </Nav>
+    </Navbar>
+  )
+}
+export default Nosotros
